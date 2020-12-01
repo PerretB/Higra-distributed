30f7b71c25b9a1f11905fffd9ec942eefbb5e318
Benjamin Perret <benjamin.perret@esiee.fr>
2020-12-01 17:21:27 +0100

    add functions select and join

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,269 @@
 #include "pybind11/pybind11.h"
 #include "higra/graph.hpp"
+#include "higra/sorting.hpp"
+#include "higra/structure/unionfind.hpp"
+#include "higra/accumulator/tree_accumulator.hpp"
 
 #define FORCE_IMPORT_ARRAY
-#include "xtensor-python/pyarray.hpp"
 
-// Example
-auto example_function(const hg::tree & tree, const xt::pyarray<double> & altitudes){
-    xt::pyarray<double> res = xt::zeros_like(altitudes);
-    for(auto n: leaves_to_root_iterator(tree, hg::leaves_it::exclude)){
-        double tmp = 1;
-        for(auto c: hg::children_iterator(n, tree)){
-            tmp *= res[c];
+#include "xtensor-python/pyarray.hpp"
+#include "xtensor-python/pytensor.hpp"
+#include "xtensor/xindex_view.hpp"
+#include "xtensor/xview.hpp"
+#include "xtensor/xnoalias.hpp"
+
+namespace py = pybind11;
+
+
+namespace hg {
+    namespace distributed {
+        template<typename T>
+        auto select(const hg::tree &tree, const xt::xexpression<T> &xselected_leaves) {
+            auto &selected_leaves = xselected_leaves.derived_cast();
+            hg_assert_1d_array(selected_leaves);
+            hg_assert_integral_value_type(selected_leaves);
+
+            // indices of the nodes in the new tree, -1 if the node is not part of the new tree
+            array_1d<index_t> new_node_index = xt::empty<index_t>({num_vertices(tree)});
+            new_node_index.fill(-1);
+
+            // at the beginning, all leaf nodes have:
+            //  - a new node index of -1 if they are not selected
+            //  - a temporary node index of 0 if they are selected
+            // at the end of iteration i:
+            //  - all nodes <= i have a valid new node index
+            //  - all nodes > i that have a child <= i with a new node index != 1 have a temporary node index of 0
+            xt::index_view(new_node_index, selected_leaves) = 0;
+            index_t num_nodes_new_tree = 0;
+            for (index_t i: leaves_to_root_iterator(tree, leaves_it::include, root_it::exclude)) {
+                if (new_node_index(i) != -1) {
+                    new_node_index(i) = num_nodes_new_tree;
+                    new_node_index(parent(i, tree)) = 0;
+                    ++num_nodes_new_tree;
+                }
+            }
+            // special case for the root
+            if (new_node_index(root(tree)) != -1) {
+                new_node_index(root(tree)) = num_nodes_new_tree;
+                ++num_nodes_new_tree;
+            }
+
+            // construct final parent and node map
+            array_1d<index_t> new_parent = xt::empty<index_t>({num_nodes_new_tree});
+            array_1d<index_t> node_map = xt::empty<index_t>({num_nodes_new_tree});
+
+            index_t count = 0;
+            for (index_t i: leaves_to_root_iterator(tree, leaves_it::include, root_it::include)) {
+                if (new_node_index(i) != -1) {
+                    new_parent(count) = new_node_index(parent(i, tree));
+                    node_map(count) = i;
+                    ++count;
+                }
+            }
+
+            return std::make_pair(hg::tree(std::move(new_parent)), std::move(node_map));
         }
-        res[n] = altitudes[n] + tmp;
+
+        template<typename T, typename T2>
+        auto
+        join(const hg::tree &tree1, const xt::xexpression<T> &xnode_map1, const xt::xexpression<T2> &xmst_weights1,
+             const hg::tree &tree2, const xt::xexpression<T> &xnode_map2, const xt::xexpression<T2> &xmst_weights2,
+             const xt::xexpression<T> &xborder_edge_sources, const xt::xexpression<T> &xborder_edge_targets,
+             const xt::xexpression<T> &xborder_edge_map, const xt::xexpression<T2> &xborder_edge_weights) {
+            auto &node_map1 = xnode_map1.derived_cast();
+            auto &mst_weights1 = xmst_weights1.derived_cast();
+            auto &node_map2 = xnode_map2.derived_cast();
+            auto &mst_weights2 = xmst_weights2.derived_cast();
+            auto &border_edge_sources = xborder_edge_sources.derived_cast();
+            auto &border_edge_targets = xborder_edge_targets.derived_cast();
+            auto &border_edge_map = xborder_edge_map.derived_cast();
+            auto &border_edge_weights = xborder_edge_weights.derived_cast();
+            hg_assert_1d_array(node_map1);
+            hg_assert_1d_array(node_map2);
+            hg_assert_1d_array(mst_weights1);
+            hg_assert_1d_array(mst_weights2);
+            hg_assert_1d_array(border_edge_sources);
+            hg_assert_1d_array(border_edge_targets);
+            hg_assert_1d_array(border_edge_map);
+            hg_assert_1d_array(border_edge_weights);
+            hg_assert_integral_value_type(node_map1);
+            hg_assert_node_weights(tree1, node_map1);
+            hg_assert_node_weights(tree2, node_map2);
+            hg_assert(mst_weights1.size() == num_vertices(tree1) - num_leaves(tree1),
+                      "mst altitudes 1 does not match with the size of tree1.");
+            hg_assert(mst_weights2.size() == num_vertices(tree2) - num_leaves(tree2),
+                      "mst altitudes 2 does not match with the size of tree2.");
+            hg_assert_same_shape(border_edge_sources, border_edge_targets);
+            hg_assert_same_shape(border_edge_sources, border_edge_targets);
+            hg_assert_same_shape(border_edge_sources, border_edge_weights);
+
+            const index_t size_tree1 = num_vertices(tree1);
+            const index_t size_tree2 = num_vertices(tree2);
+            const index_t num_leaves_tree1 = num_leaves(tree1);
+            const index_t num_leaves_tree2 = num_leaves(tree2);
+            index_t num_border_edges = border_edge_sources.size();
+            index_t num_leaves_join = num_leaves_tree1 + num_leaves_tree2;
+
+            using value_type = typename T2::value_type;
+            using pair = std::pair<value_type, index_t>;
+            pair infinity = pair((std::numeric_limits<value_type>::has_infinity) ?
+                                 std::numeric_limits<value_type>::infinity() :
+                                 std::numeric_limits<value_type>::max(),
+                                 std::numeric_limits<index_t>::max());
+
+            const auto less = [](const pair &p1, const pair &p2) {
+                return (p1.first < p2.first) ||
+                       ((p1.first == p2.first) && (p1.second < p2.second));
+            };
+
+            const auto attribute_child_one_leaf_node = [](const auto &tree, const index_t shift) {
+                array_2d<index_t> attr({num_vertices(tree), 2}, -1);
+                xt::noalias(xt::view(attr, xt::range(0, (index_t) num_leaves(tree)), 0)) =
+                        xt::arange(shift, shift + (index_t) num_leaves(tree));
+
+                for (auto i: leaves_to_root_iterator(tree, leaves_it::exclude, root_it::include)) {
+                    attr(i, 0) = attr(child(0, i, tree), 0);
+                    if (num_children(i, tree) == 2) {
+                        attr(i, 1) = attr(child(1, i, tree), 0);
+                    }
+                }
+                return attr;
+            };
+
+            const auto child_one_leaf_tree1 = attribute_child_one_leaf_node(tree1, 0);
+            const auto child_one_leaf_tree2 = attribute_child_one_leaf_node(tree2, num_leaves_tree1);
+
+            const auto sorted_edge_indices = hg::stable_arg_sort(border_edge_weights);
+
+            // disjoint forest
+            union_find uf(num_leaves_join);
+
+            // mapping canonical nodes of the disjoint forest to their respective tree root
+            array_1d<index_t> roots = xt::arange<index_t>(num_leaves_join);
+
+            // new parent mapping : size is the worst case
+            array_1d<index_t> new_parents = xt::empty<index_t>({size_tree1 + size_tree2 + num_border_edges});
+            // new node mapping, leaf nodes are mapped to their unique global graph vertex identifier
+            // internal nodes are mapped to the unique global edge identifier of their mst building edge
+            array_1d<index_t> new_node_map = xt::empty_like(new_parents);
+            // initialize node mapping of leaves
+            xt::noalias(xt::view(new_node_map, xt::range(0, num_leaves_tree1))) =
+                    xt::view(node_map1, xt::range(0, num_leaves_tree1));
+            xt::noalias(xt::view(new_node_map, xt::range(num_leaves_tree1, num_leaves_join))) =
+                    xt::view(node_map2, xt::range(0, num_leaves_tree2));
+
+            // current number of nodes in the new tree
+            index_t num_nodes = num_leaves_join;
+
+            // current indices in the three edge sets
+            index_t index_tree1 = num_leaves_tree1;
+            index_t index_tree2 = num_leaves_tree2;
+            index_t index_edge = 0;
+
+            // while an edge set is not empty
+            while (index_tree1 < size_tree1 || index_tree2 < size_tree2 || index_edge < num_border_edges) {
+                const auto ei = sorted_edge_indices(index_edge);
+                index_t canonical1 = -1;
+                index_t canonical2 = -1;
+                index_t mst_edge;
+
+                pair iw_tree1 = (index_tree1 < size_tree1) ?
+                                pair(mst_weights1(index_tree1 - num_leaves_tree1), node_map1(index_tree1)) :
+                                infinity;
+                pair iw_tree2 = (index_tree2 < size_tree2) ?
+                                pair(mst_weights2(index_tree2 - num_leaves_tree2), node_map2(index_tree2)) :
+                                infinity;
+                pair iw_edge = (index_edge < num_border_edges) ?
+                               pair(border_edge_weights(ei), border_edge_map(ei)) :
+                               infinity;
+
+                // smallest element is a border edge : normal Kruskal step
+                if (less(iw_edge, iw_tree1) && less(iw_edge, iw_tree2)) {
+
+                    auto source = border_edge_sources(ei);
+                    auto target = border_edge_targets(ei);
+                    canonical1 = uf.find(source);
+                    canonical2 = uf.find(target + num_leaves_tree1);
+                    mst_edge = iw_edge.second;
+                    ++index_edge;
+                } else { // smallest element is a node of a tree
+
+                    if (less(iw_tree1, iw_tree2)) {
+                        mst_edge = iw_tree1.second;
+                        canonical1 = uf.find(child_one_leaf_tree1(index_tree1, 0));
+                        if (child_one_leaf_tree1(index_tree1, 1) != -1) {
+                            canonical2 = uf.find(child_one_leaf_tree1(index_tree1, 1));
+                        }
+                        ++index_tree1;
+                    } else {
+                        mst_edge = iw_tree2.second;
+                        canonical1 = uf.find(child_one_leaf_tree2(index_tree2, 0));
+                        if (child_one_leaf_tree2(index_tree2, 1) != -1) {
+                            canonical2 = uf.find(child_one_leaf_tree2(index_tree2, 1));
+                        }
+                        ++index_tree2;
+                    }
+                }
+
+                if (canonical1 != canonical2) {
+                    auto root1 = roots(canonical1);
+                    auto new_node = num_nodes;
+                    ++num_nodes;
+                    new_parents[root1] = new_node;
+                    new_node_map(new_node) = mst_edge;
+
+                    if (canonical2 == -1) {
+                        roots(canonical1) = new_node;
+                    } else {
+                        auto root2 = roots(canonical2);
+                        new_parents(root2) = new_node;
+                        auto new_root = uf.link(canonical1, canonical2);
+                        roots(new_root) = new_node;
+                    }
+                }
+            }
+
+            new_parents(num_nodes - 1) = num_nodes - 1;
+
+            return std::make_pair(
+                    hg::tree(xt::view(new_parents, xt::range(0, num_nodes))),
+                    xt::eval(xt::view(new_node_map, xt::range(0, num_nodes))));
+        }
     }
-    return res;
 }
 
+
+using namespace hg;
+
+
 // Python Module and Docstrings
-PYBIND11_MODULE(higra_distributed, m)
-{
+PYBIND11_MODULE(higra_distributed, m) {
     xt::import_numpy();
 
     m.doc() = R"pbdoc(
         An example higra extension
     )pbdoc";
 
-    m.def("example_function", example_function, "An example function.");
-    
+    m.def("select", [](const hg::tree &tree, const xt::pyarray<hg::index_t> &selected_leaves) {
+              hg_assert(xt::amin(selected_leaves)() >= 0 && xt::amax(selected_leaves)() < (index_t) num_leaves(tree),
+                        "Invalid leaf index.");
+              auto res = hg::distributed::select(tree, selected_leaves);
+              return py::make_tuple(std::move(res.first), std::move(res.second));
+          }, "Select a subtree induced by a subset of leaves of the input tree.",
+          py::arg("tree"),
+          py::arg("selected_leaves"));
+
+    m.def("join",
+          [](const hg::tree &tree1, const xt::pyarray<hg::index_t> &node_map1, const xt::pyarray<double> &mst_weights1,
+             const hg::tree &tree2, const xt::pyarray<hg::index_t> &node_map2, const xt::pyarray<double> &mst_weights2,
+             const xt::pyarray<hg::index_t> &border_edge_sources, const xt::pyarray<hg::index_t> &border_edge_targets,
+             const xt::pyarray<hg::index_t> &border_edge_map, const xt::pyarray<double> &border_edge_weights) {
+
+              auto res = hg::distributed::join(tree1, node_map1, mst_weights1,
+                                               tree2, node_map2, mst_weights2,
+                                               border_edge_sources, border_edge_targets,
+                                               border_edge_map, border_edge_weights);
+              return py::make_tuple(std::move(res.first), std::move(res.second));
+          }, "Join two trees by their border edges.");
+
 }
--- tests/test_extension.py
+++ tests/test_extension.py
@@ -10,55 +10,86 @@ class ExampleTest(unittest.TestCase):
         """
         base graph is
 
-        (0)-- 0 --(1)-- 2 --(2)
-         |         |         |
-         6         6         0
-         |         |         |
-        (3)-- 0 --(4)-- 4 --(5)
-         |         |         |
-         5         5         3
-         |         |         |
-        (6)-- 0 --(7)-- 1 --(8)
+        (0)-- 3 --(1)-- 12--(2)-- 2 --(3)-- 8 --(4)-- 1 --(5)
+         |         |         |         |         |         |
+         16        15        14        11        10        7
+         |         |         |         |         |         |
+        (6)-- 6 --(7)-- 13--(8)-- 4 --(9)-- 9 --(10)- 5 --(11)
 
-        Minima are
-        A: (0, 1)
-        B: (3, 4)
-        C: (2, 5)
-        D: (6, 7)
+
+
 
         BPT:
-
-
-
-
-        4                 +-------16------+
-                          |               |
-        3         +-------15-----+        |
-                  |              |        |
-        2     +---14--+          |        |
-              |       |          |        |
-        1     |       |       +--13-+     |
-              |       |       |     |     |
-        0   +-9-+   +-10+   +-12+   |   +-11+
-            +   +   +   +   +   +   +   +   +
-            0   1   2   5   6   7   8   3   4
+                                        +------22-----+
+                                        |             |
+                               +--------21----+       |
+                               |              |       |
+                        +------20-----+       |       |
+                        |             |       |       |
+                  +-----19----+       |       |       |
+                  |           |       |       |       |
+              +---18--+       |       |       |       |
+              |       |       |       |       |       |
+            +-12+   +-16+   +-13+   +-15+   +-14+   +-17+
+            +   +   +   +   +   +   +   +   +   +   +   +
+            4   5   10  11  2   3   8   9   0   1   6   7
 
 
         :return:
         """
 
-        g = hg.get_4_adjacency_graph((3, 3))
-        edge_weights = np.asarray((0, 6, 2, 6, 0, 0, 5, 4, 5, 3, 0, 1))
+        g = hg.get_4_adjacency_graph((2, 6))
+        edge_weights = np.asarray((3, 16, 12, 15, 2, 14, 8, 11, 1, 10, 7, 6, 13, 4, 9, 5))
 
         return hg.bpt_canonical(g, edge_weights)
 
-    def test_example_function(self):
-        tree, altitudes = ExampleTest.get_test_tree()
-        
-        result = m.example_function(tree, altitudes)
+    def test_select(self):
+        base_tree, altitudes = ExampleTest.get_test_tree()
 
-        expected_result = (0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 2., 5., 4.)
-        self.assertTrue(np.allclose(result, expected_result))
+        tree, node_map = m.select(base_tree, (0, 1, 6, 7))
+        ref_parents = (4, 4, 5, 5, 6, 7, 7, 7)
+        ref_node_map = (0, 1, 6, 7, 14, 17, 21, 22)
+        self.assertTrue(np.all(tree.parents() == ref_parents))
+        self.assertTrue(np.all(node_map == ref_node_map))
+
+        tree, node_map = m.select(base_tree, (2, 3, 8, 9))
+        ref_parents = (4, 4, 5, 5, 6, 7, 7, 8, 9, 9)
+        ref_node_map = (2, 3, 8, 9, 13, 15, 19, 20, 21, 22)
+        self.assertTrue(np.all(tree.parents() == ref_parents))
+        self.assertTrue(np.all(node_map == ref_node_map))
+
+        tree, node_map = m.select(base_tree, (10, 5, 11, 4))
+        ref_parents = (4, 4, 5, 5, 6, 6, 7, 8, 9, 10, 10)
+        ref_node_map = (4, 5, 10, 11, 12, 16, 18, 19, 20, 21, 22)
+        self.assertTrue(np.all(tree.parents() == ref_parents))
+        self.assertTrue(np.all(node_map == ref_node_map))
+
+
+    def test_join(self):
+
+
+        tree1 = hg.Tree((2, 3, 4, 4, 5, 6, 6))
+        node_map1 = np.asarray((3, 9, 4, 13, 7, 2, 3))
+        mst_edge_weights1 = np.asarray((2, 4, 11, 12, 15))
+
+        tree2 = hg.Tree((2, 3, 4, 4, 4))
+        node_map2 = np.asarray((4, 10, 8, 15, 10))
+        mst_edge_weights2 = np.asarray((1, 5, 7))
+
+        border_edge_sources = np.asarray((0, 1))
+        border_edge_targets = np.asarray((0, 1))
+        border_edge_map = np.asarray((6, 14))
+        border_edge_weights = np.asarray((8, 9))
+
+        tree, node_map = m.join(tree1, node_map1, mst_edge_weights1,
+                                tree2, node_map2, mst_edge_weights2,
+                                border_edge_sources, border_edge_targets,
+                                border_edge_map, border_edge_weights)
+
+        ref_parents = (5, 6, 4, 7, 8, 9, 10, 8, 9, 10, 11, 12, 12)
+        ref_node_map = (3, 9, 4, 10, 8, 4, 13, 15, 10, 6, 14, 2, 3)
+        self.assertTrue(np.all(tree.parents() == ref_parents))
+        self.assertTrue(np.all(node_map == ref_node_map))
 
 if __name__ == '__main__':
     unittest.main()
