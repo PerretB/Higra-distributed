e42932b845723c362c4033ff2ef6c4435585d69f
Benjamin Perret <benjamin.perret@esiee.fr>
2021-03-06 10:02:47 +0100

    add license info and cleanup

--- higra_distributed/__init__.py
+++ higra_distributed/__init__.py
@@ -1,2 +1,11 @@
+############################################################################
+# Copyright ESIEE Paris (2021)                                             #
+#                                                                          #
+# Contributor(s) : Benjamin Perret                                         #
+#                                                                          #
+# Distributed under the terms of the CECILL-B License.                     #
+#                                                                          #
+# The full license is in the file LICENSE, distributed with this software. #
+############################################################################
 
 from .distributed import *
---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+############################################################################
+# Copyright ESIEE Paris (2021)                                             #
+#                                                                          #
+# Contributor(s) : Benjamin Perret                                         #
+#                                                                          #
+# Distributed under the terms of the CECILL-B License.                     #
+#                                                                          #
+# The full license is in the file LICENSE, distributed with this software. #
+############################################################################
